7ee8ae792e2042190b456287ad9e6e89de2c1b70
kwamebessah <kwamebessah@gmail.com>
2023-10-14 02:40:05 -0700

    new

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
+ * main - Prints the alphabet.
  *
- * */
+ * Return: Always 0 (Success)
+ *
+*/
 
 int main(void)
 {
