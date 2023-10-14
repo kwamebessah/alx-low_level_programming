98aca7212503f79ba428396675e76e8d03a0935c
kwamebessah <kwamebessah@gmail.com>
2023-10-14 02:52:33 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,10 +5,11 @@
 #include <string.h>
 
 /**
- * main - Prints the alphabet without q and e.
- *
- * Return: Always 0 (Success)
- *
+ *
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ *
 */
 
 int main(void)
