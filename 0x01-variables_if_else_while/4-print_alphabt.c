aa6293ef7477c6923099189d16fba021bbc4d873
kwamebessah <kwamebessah@gmail.com>
2023-10-14 03:00:06 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,17 +5,15 @@
 #include <string.h>
 
 /**
- *
  * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
- *
 */
 
 int main(void)
 {
 	int i;
-	
+
 	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
@@ -23,8 +21,8 @@ int main(void)
 			putchar(i);
 		}
 	}
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
